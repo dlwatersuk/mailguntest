1a49a91d9ffea3a48f0eda5d40658df9f508b683
Daniel <me@dlwaters.co.uk>
2022-02-14 23:25:41 +0000

    initial commit

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    public function __construct() {
+
+    }
+
+    public function form() {
+        return view('mailform');
+    }
+
+    public function process() {
+        
+    }
+}
---
+++ app/Mail/Message.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class Message extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data = [
+        'subject' => null,
+        'content' => null
+    ];
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->subject($this->data->subject)
+            ->view('view.name', [
+            $this->data->content
+        ]);
+    }
+}
